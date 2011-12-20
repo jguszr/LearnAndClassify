ba7783964bd7cc30fb229b3e3b11eb8083638bb7
rosjose <rosjose@SPMADWS1331.es.db.com>
2011-12-20 11:16:14 -0200

    getting started

---
+++ LearnAndClassify2/src/simple/loader/textLoader.py
@@ -0,0 +1,97 @@
+'''
+Created on 19/12/2011
+
+@author: josegustavozagatorosa
+'''
+# -*- coding: cp1252 -*-
+
+import os
+
+RAW_DATA_PATH = "/Users/josegustavozagatorosa/Documents/workspace/LearnAndClassify/rawData/"
+CONTEXT_PATH = "/Users/josegustavozagatorosa/Documents/workspace/LearnAndClassify/contextData/"
+
+KEY_ID = "@@@k:"
+LIST_VALUES_ID = "@@@Values:"
+LIST_SIZE = "@@@size:"
+
+def loadRaw(tag,container,fileName,chunkSize=1000, startingAt=0):
+
+    rawFile = open(RAW_DATA_PATH+fileName,'r')
+        
+    data = []
+    i,index = 0,0
+    
+    for line in rawFile:
+        if i>=startingAt:
+            if index<chunkSize:
+                data.append(line)
+                index += 1
+            else:
+                break
+        i+=1
+        
+    container[tag]=data
+    rawFile.close()
+
+    return container
+
+def assertContextFile():
+    if not os.path.exists(CONTEXT_PATH):
+        os.makedirs(CONTEXT_PATH)
+
+def saveContext(context,filename):
+    assertContextFile()
+    toSave = open(CONTEXT_PATH+filename,'w')
+    for k,v in context.iteritems():
+        toSave.write(KEY_ID + k )
+        toSave.write(LIST_SIZE + str(len(v)) + "\n")
+        [(l, toSave.write(l)) for l in v]
+    toSave.close()
+
+def isKeyId(line):
+    return line.startswith(KEY_ID)
+
+def extractKey(line):
+    r = line.rsplit("k:")[1]
+    return r.rsplit("@")[0]
+
+
+def getValuesSize(line):
+    return line.rsplit(LIST_SIZE)[1]
+
+def loadContext(filename):
+    
+    newContext = {}
+    toLoad = open(CONTEXT_PATH+filename,'r')
+    k = ''
+    l = 'x'
+    while l<>'':
+        data=[]
+        l = toLoad.readline()
+        lsize = 0
+        if isKeyId(l):
+            k = extractKey(l)
+            lsize = int(getValuesSize(l))
+        i=0
+        while i<lsize:
+            l = toLoad.readline()
+            data.append(l)
+            i+=1
+        if k <>'' and len(data)>0:
+            newContext.update({k:data})
+    toLoad.close()
+    return newContext    
+    
+def basicTest():
+    cont = {}
+    cont = loadRaw("bacon",cont,"pg46.txt",100,20)
+    saveContext(cont,"batman.raw")
+    cont2 = loadContext("batman.raw")
+    cont2.update(loadRaw("cheese",cont2,"pg46.txt",10,200))
+    saveContext(cont2,"robin.raw")
+    
+    finalTest = loadContext("robin.raw")
+    for k,v in finalTest.iteritems():
+        print k,
+        print v
+    return finalTest
---
+++ LearnAndClassify2/src/simple/probabilities.py
@@ -0,0 +1,83 @@
+'''
+Created on 19/12/2011
+
+@author: josegustavozagatorosa
+'''
+# -*- coding: cp1252 -*-
+
+from collections import OrderedDict
+import operator
+
+
+#from collections import OrderedDict
+
+
+
+context = {"A" : [0.0,0.0,0.0,1.0,1.0,1.0,1.0,2.0,2.0,2.0,2.0,2.0],
+           "B" : [3.0,3.0,3.0,4.0,4.0,4.0,4.0,1.0,6.0,7.0,6.0,5.0],
+           "C" : [3.0,0.0,0.0,4.0,4.0,1.0,1.0,1.0,7.0,7.0,8.0,5.0]}
+
+
+def setNewContext(theNew):
+    context = theNew 
+
+def frequencies(s):
+    return ((a, s.count(a)) for a in set(s))
+
+
+def individualProbabilites(key):
+    m = {}
+    [m.update({k:v}) for k,v in frequencies(context[key])]
+
+    return m
+
+def countAllContext():
+    total = 0
+    for k in context.iterkeys():
+        total += len(context[k])
+    return total
+
+def itemProbability(toTest,givenContext):
+    return context[givenContext].count(toTest) / float(len(context[givenContext]))
+
+def contextProbability(key):
+    return len(context[key]) / float(countAllContext())
+    
+
+def naiveBayes(toTest,givenContext):
+    return (contextProbability(givenContext) * combinedItemsProbability(toTest,givenContext))/ float(countAllContext())
+
+def combinedItemsProbability(toTest,givenContext):
+    
+    tp = 0
+    for i in toTest:
+        tp += itemProbability(i,givenContext)
+    return tp
+
+def classify(toTest):
+    resp = {}
+    for k in context.iterkeys():
+        resp[k] = naiveBayes(toTest,k)
+    return resp
+
+def maxLikelyHood(toTest):
+    
+    k = max(toTest, key=toTest.get)
+    return [(k,toTest[k])]
+    
+def basicTest():
+    print len(context["A"])
+    print individualProbabilites("B")
+    print "contextSize = ", countAllContext()
+    print "context relevance = ", contextProbability("A")
+    print naiveBayes([0.0,0.0],"C")
+    for k in context.iterkeys():
+        print k, " = ", itemProbability(1.0,k)
+    
+def test():
+    print "classify \n"
+    cls = OrderedDict(sorted(classify([0.0,0.0]).iteritems(),key=operator.itemgetter(1),reverse=False))
+    for x,y in cls.iteritems():
+        print x," = ", y
+
+    print "maximum likelihood = ",maxLikelyHood(cls)
